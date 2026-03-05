b91e3ba989c34ed0e9a7332c4519a48e77165450
Wellington Lopes <wellington.lopes.tw@gmail.com>
2026-03-04 21:44:16 -0300

    docs: add godoc to ClientUTC

--- pkg/clock/client_utc.go
+++ pkg/clock/client_utc.go
@@ -2,12 +2,18 @@ package clock
 
 import "time"
 
+// ClientUTC provides time operations in UTC timezone.
 type ClientUTC struct{}
 
+// NewClientUTC returns a new ClientUTC instance.
 func NewClientUTC() *ClientUTC {
 	return &ClientUTC{}
 }
 
+// Now returns the current time in UTC.
+//
+// Returns:
+//   - time.Time: The current UTC time.
 func (c *ClientUTC) Now() time.Time {
 	return time.Now().UTC()
 }
